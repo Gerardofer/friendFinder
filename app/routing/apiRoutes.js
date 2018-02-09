f89597d6904974ce7b46d3994e6a9fa44056f701
Gerardo Fernandez <gerardojose13@gmail.com>
2018-02-08 23:17:26 -0800

    fixes routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,25 +1,13 @@
-const express = require("express");
-const app = express();
 const friends = require('../data/friends.js');
-
 
 module.exports = function(app) {
     app.get("/api/friends", (req, res) => {
         res.json(friends)
     });
 
-    // app.post("/api/friends", function(req, res) {
-    //     // req.body hosts is equal to the JSON post sent from the user
-    //     // This works because of our body-parser middleware
-    //     var friend = req.body;
-    //     // Using a RegEx Pattern to remove spaces from newCharacter
-    //     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    //     friend.routeName = friend.name.replace(/\s+/g, "").toLowerCase();
+    app.post("/api/friends", function(req, res) {
+        friends.push(req.body);
+        res.json(true);
 
-    //     console.log(friend);
-
-    //     friends.push(friend);
-
-    //     res.json(friend);
-    // });
+    });
 }
